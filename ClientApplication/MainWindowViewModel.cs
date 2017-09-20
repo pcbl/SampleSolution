5a86f9a33a1c19ef67e5bf1671cc5a08e8d58c3f
Pedro Castelo Branco <pedro_castelo@hotmail.com>
2017-09-20 14:21:41 +0000

    added WPF client Fixed nchar to nvarchar

---
+++ ClientApplication/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApplication.ProductDataService;
+
+namespace ClientApplication
+{
+    public class MainWindowViewModel
+    {
+        private readonly ProductServiceClient _service;
+
+        public MainWindowViewModel()
+        {
+            _service = new ProductServiceClient();
+            Products = new ObservableCollection<Product>();
+
+            Load = new RelayCommand((parameter) =>
+            {
+                Products.Clear();
+                var allproducts = _service.GetProducts();
+                foreach (var product in allproducts)
+                {
+                    Products.Add(product);
+                }
+            });
+        }
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        public RelayCommand Load { get; set; }
+
+    }
+}
---
+++ ClientApplication/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ClientApplication
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object>  execute, Func<object, bool> canExecute=null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+                return _canExecute(parameter);
+            else
+                return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
